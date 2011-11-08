d015cc601f6461a0d2fc9edd2e930462e6bdd4a0
Tyler Mark Good <tyler@tylergood.net>
2011-11-08 08:20:56 -0500

    updated GNU License on plugin

--- xnormal_plugin/src/bake_layer_tool/quick_edit_window.py
+++ xnormal_plugin/src/bake_layer_tool/quick_edit_window.py
@@ -1,7 +1,33 @@
 '''
-Created on Jul 5, 2011
+    tgood_xn_exporter
+    0.5
+    
+    copyright (c) 2010 Tyler Good
+    This is free software: free as in free speech, and free as in free beer.
+    
+    This file is quick_edit_window.py
+    This Package is bake_layer_tool
 
-@author: Tyler
+    bake_layer_tool is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    bake_layer_tool is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with bake_layer_tool.  If not, see <http://www.gnu.org/licenses/>.
+    
+    Description:
+        script created by Tyler Good for running xnormal straight from maya
+        and baking maps
+
+    please email me if you have any questions, comments, critques, or requests
+    tyler@tylergood.net
+    
 '''
 
 import pymel.core as pmc
